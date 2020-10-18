4f666df6605891a886336a73124125dfdcdfa579
Muditalbet <muditalbet@gmail.com>
2020-10-18 11:14:54 +0530

    Making some code changes so that portfolio template become more precise and good

--- js/demo.js
+++ js/demo.js
@@ -6,8 +6,8 @@
 // This can be used to set the Particles Effects. Check README for more details!
 document.addEventListener('DOMContentLoaded', function () {
   particleground(document.getElementById('particles'), {
-    dotColor: '#5cbdaa',
-    lineColor: '#5cbdaa'
+    dotColor: '#fad3cf',
+    lineColor: '#2470a0'
   });
   var intro = document.getElementById('intro');
   intro.style.marginTop = - intro.offsetHeight / 2 + 'px';
